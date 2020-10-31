670a075f31126e5fe6fd443cb4a3481c4f1c0ad7
Fahrih Fatahilah <painchikusudo@gmail.com>
2020-10-31 15:52:23 +0800

    PBOIF2

---
+++ src/kelipatann/Kelipatann.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelipatann;
+
+/**
+ *
+ * @author Fahrih Fatahilah
+ * Kelas : IF-2 / PBO-IF2
+ * Nim   : 10116405
+ * 
+ */
+public class Kelipatann {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     for (double i = 3.5; i <= 35; i += 3.5){
+         System.out.println(i);
+     }
+    }
+}
